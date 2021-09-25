3c190b3e9b83ced43081b63ca9675cf47b04741d
serg155alternate <serg1557733@i.ua>
2021-09-25 18:51:03 +0300

    second step

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
-/* 
-function prefill(n, v) {
-    if ( isNaN(n) || n < 0 || !Number.isInteger(n)|| typeof n === 'boolean') {
-          let typeError = new TypeError();
-      
-          typeError.name = "TypeError";
-          typeError.message =  n + " is invalid";
-          throw typeError;  
-      } 
-   
-       else if (v === undefined) {
-         let arrayN = Array.from({ length: n }, () => undefined);
-        
-         return arrayN;
-      } else {
-         let arrayN = Array.from({ length: n }, () => v);
-        
-         return arrayN;
-      }
- }
 
- console.log(prefill(1, -3)); */
+/*  
+ function rentalCarCost(d) {
+  if (d >= 7) {
+    return (d * 40) - 50;
+  } else if ( d >= 3 && d < 7) {
+    return (d * 40) - 20;
+  } else return d * 40;
+} */
+
+
 
 /*  function sequence(n, pattern) {
    let arr = new Array();
@@ -34,20 +23,47 @@ function prefill(n, v) {
 
   //console.log(sequence(5, []));
   console.log(sequence(5, (x, idx) => idx%2)); */
-/* 
-  function createFunctions(n) {
-    let callbacks = [];
-  
-    for (var i=0; i<n; i++) {
-      callbacks.push(function() {
-        return i;
-      });
+
+/*   function basicOp(operation, value1, value2)
+  {
+    if (operation === '+') {
+      return value1 + value2;
+    } else if (operation === '-') {
+      return value1 - value2;
     }
-    
-    return callbacks;
+     else if (operation === '*') {
+      return value1 * value2;
+    }
+     else if (operation === '/') {
+      return value1 / value2;
+    } else {
+        console.log('Enter correct operation');
+    }
   } */
 
-  function createSecretHolder(secret){
+
+/* 
+  function array(arr){
+    let arrSplit = arr.split(',');
+   if (arrSplit.length < 3) {
+     return null;
+   } else {
+     return arrSplit.slice( 1, arrSplit.length - 1 ).join(' ');
+   }
+} */
+
+
+/* function getMiddle(s)
+{
+  if (s.length % 2 == 0) {
+    return s[s.length / 2 - 1] + s[s.length / 2];
+  } else {
+    return s[Math.floor(s.length / 2)] ;
+  }
+} */
+
+
+/*   function createSecretHolder(secret){
     return { 
       secret: secret,
       setSecret: function(secret){
@@ -64,4 +80,30 @@ obj = createSecretHolder(5);
 console.log(obj);
 console.log(obj.getSecret()) ;//5
 obj.setSecret(2);
-console.log(obj.getSecret());//2
+console.log(obj.getSecret());//2 */
+
+
+/* 
+Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+You have to validate input:
+v can be anything (primitive or otherwise)
+if v is ommited, fill the array with undefined
+if n is 0, return an empty array
+if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function. */
+
+function prefill(n, v) {
+    console.log(!Number.isInteger(n));
+
+  if ( isNaN(n)|| typeof n === 'boolean' || !isFinite(n) || !Number.isInteger(n) ) {
+        throw new TypeError(`${n} is invalid`);  
+    } else if (n == 0) {
+      return [ ];
+     } else if ( n < 0 ) {
+         throw new RangeError(`${n} is invalid`);
+     } else {
+       return new Array(n).fill(v);
+    }
+}
+
+console.log(prefill(1.2 , 1));
